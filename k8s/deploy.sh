9028e76cf863075e2f8f252aaeb885a82da2dd94
Bofh82 <rodionov_s@inbox.ru>
2023-02-08 21:31:27 +0300

    Добавил переменную PROFILE_SERVICE_URL

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -8,4 +8,5 @@ sed -r "s/ENV_DATASOURCE_HOST/$4/g;" |
 sed -r "s/ENV_DATASOURCE_PORT/\"$5\"/g;" |
 sed -r "s/ENV_SERVICE_NAME/$6/g;" |
 sed -r "s/ENV_SERVICE_PORT/\"$7\"/g;" |
+sed -r "s/ENV_PROFILE_SERVICE_URL/\"$8\"/g;" |
 kubectl apply -f -
